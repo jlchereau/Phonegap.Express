c9e9e6d465160d8b4c6fa519895fdd242620d150
jlchereau <jlchereau@msn.com>
2014-05-12 18:10:08 +0100

    Logging

--- api/config/configuration.js
+++ api/config/configuration.js
@@ -1,11 +1,12 @@
-var nconf = require('nconf');
+var nconf = require('nconf'),
+    path = require('path');
 
 function Config(){
     nconf.argv().env('_');
     var environment = nconf.get('NODE:ENV') || 'development';
-    console.log('Phonegap.Express: environment is ' + environment)
-    nconf.file(environment, 'config/' + environment + '.json');
-    nconf.file('default', 'config/default.json');
+    console.log('Phonegap.Express: environment is ' + environment);
+    nconf.file(environment, path.join(__dirname, '../../config/' + environment + '.json'));
+    nconf.file('default', path.join(__dirname, '../../config/default.json'));
 }
 
 Config.prototype.get = function(key) {
